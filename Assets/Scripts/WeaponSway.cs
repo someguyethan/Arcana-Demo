c387f17767b7a55d93209ffe9252b00168a47c60
Ethan Kowalchuk <ethan.kowalchuk@ontariotechu.net>
2021-09-26 18:13:52 -0400

    Added Weapon Sway

---
+++ Assets/Scripts/WeaponSway.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSway : MonoBehaviour
+{
+    [Header("Sway Settings")]
+    [SerializeField] private float smooth;
+    [SerializeField] private float swayMulti;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * swayMulti;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * swayMulti;
+
+        Quaternion rotX = Quaternion.AngleAxis(-mouseY, Vector3.right);
+        Quaternion rotY = Quaternion.AngleAxis(mouseX, Vector3.up);
+
+        Quaternion targetRot = rotX * rotY;
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRot, smooth * Time.deltaTime);
+    }
+}
